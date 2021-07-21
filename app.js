c6915bb0cec0176ab7e145dd341b8e9faf570548
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-07-21 09:39:04 +0200

    safari support

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
 
+const modelCanvas = document.getElementById("model-three");
+
+console.log(navigator.platform);
+console.log(navigator.userAgent);
+
+var video = document.getElementById("model-three");
+// var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -21,15 +39,15 @@ function cameraStart() {
         });
 }
 
-// // Take a picture when cameraTrigger is tapped
-// cameraTrigger.onclick = function() {
-//     cameraSensor.width = cameraView.videoWidth;
-//     cameraSensor.height = cameraView.videoHeight;
-//     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-//     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-//     cameraOutput.classList.add("taken");
-//     // track.stop();
-// };
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
