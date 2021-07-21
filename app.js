800fa14683a59bcb3a02ba9d4441e55ecf5fc0f7
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-07-21 11:51:40 +0200

    add object with three

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+
 // Set constraints for the video stream
 var constraints = { video: { facingMode: { exact: "environment" } }, audio: false };
 var track = null;
@@ -13,8 +15,24 @@ const modelCanvas = document.getElementById("model-three");
 console.log(navigator.platform);
 console.log(navigator.userAgent);
 
-var video = document.getElementById("model-three");
+const threeModel = document.getElementById("model-three");
 // var video = document.querySelector("#videoElement");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth / window.window.innerHeight, 0.1, 1000);
+camera.position.set(0, 10., 50.);
+
+const renderer = new THREE.WebGLRenderer( { canvas: modelCanvas, alpha: true} );
+renderer.setSize( window.innerWidth, window.innerHeight);
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setClearColor( 0x000000, .5 );
+renderer.shadowMap.enabled = true;
+
+//Adding Controls
+console.log(camera, renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+console.log(controls);
 
 if (navigator.platform === "MacIntel" || navigator.platform === "MacApple") {
     console.log("u're on a Mac!");
@@ -24,7 +42,7 @@ if (navigator.platform === "MacIntel" || navigator.platform === "MacApple") {
         video.srcObject = stream;
         })
         .catch(function (err0r) {
-        console.log("Something went wrong!");
+        console.log("Something went wrong! - " + err0r);
         });
     }
 } else if (navigator.platform === "iPhone") {
@@ -44,6 +62,25 @@ if (navigator.platform === "MacIntel" || navigator.platform === "MacApple") {
 } else {
     console.log("unsupported platform" + navigator.platform);
 }
+
+// adding any object at all
+const material = new THREE.MeshStandardMaterial({color : "white"});
+
+// adding a baseplane
+const plane = new THREE.BoxGeometry(10, 10);
+const planeMesh = new THREE.Mesh(plane, material);
+// planeMesh.rotation.y = .5;
+// planeMesh.rotation.x = -.5*Math.Pi;
+scene.add(planeMesh);
+
+// add light
+const light = new THREE.HemisphereLight("white", "blue", 1.);
+scene.add(light);
+const dirLight = new THREE.DirectionalLight("green", .5);
+dirLight.castShadow=true;
+scene.add(dirLight);
+
+renderer.render( scene, camera );
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
